5ed809602b618a6e6cef4c01260c357a2aaa62e8
Mark Watsham <mark@watsham.net>
2023-07-18 14:33:03 +0100

    Initial commit

---
+++ dynamodb_vcenter_item.py
@@ -0,0 +1,34 @@
+class TableItem:
+    def __init__(self):
+        self.item = {}
+
+
+class VcenterItem(TableItem):
+    def __init__(self):
+
+        super().__init__()
+
+
+class DatacenterItem(TableItem):
+    def __init__(self):
+        super().__init__()
+
+
+class ComputeClusterItem(TableItem):
+    def __init__(self):
+        super().__init__()
+
+
+class DatastoreItem(TableItem):
+    def __init__(self):
+        super().__init__()
+
+
+class NetworkItem(TableItem):
+    def __init__(self):
+        super().__init__()
+
+
+class TemplateItem(TableItem):
+    def __init__(self):
+        super().__init__()
